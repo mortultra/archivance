c35e4a600207bee8583e76cb80454ddb578b2aa6
andrewcampbell <awr.campbell@gmail.com>
2021-05-24 21:17:22 -0400

    added styling to DisplayItems for spacing

--- src/DisplayItems.js
+++ src/DisplayItems.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const DisplayItems = (props) => {
   const {invArray} = props;
   return (
@@ -7,19 +9,21 @@ const DisplayItems = (props) => {
           return(
             <div key={items.key}>
               <h2>{items.itemName}</h2>
-              <img className="archiveImg" src={items.img} alt="" />
-              <div className="infoFlex">
-                <h3>{items.season}</h3>
-                <h4>Description</h4>
-                <p>{items.description}</p>
-                <h4>Materials</h4>
-                <ul>
-                  <li>{items.materials}</li>
-                </ul>
-                <h4>Care</h4>
-                <ul>
-                  <li>{items.care}</li>
-                </ul>
+              <div className="archiveFlex">
+                <img className="archiveImg" src={items.img} alt="" />
+                <div className="archiveInfo">
+                  <h3>{items.season}</h3>
+                  <h4>Description</h4>
+                  <p>{items.description}</p>
+                  <h4>Materials</h4>
+                  <ul>
+                    <li>{items.materials}</li>
+                  </ul>
+                  <h4>Care</h4>
+                  <ul>
+                    <li>{items.care}</li>
+                  </ul>
+                </div>
               </div>
             </div>
             )
