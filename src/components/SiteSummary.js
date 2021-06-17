06c76dd9afbe6913e34c8f009f2ea4649e43ab98
andrewcampbell <awr.campbell@gmail.com>
2021-06-17 18:49:02 -0400

    changed SiteSummary description

--- src/components/SiteSummary.js
+++ src/components/SiteSummary.js
@@ -3,7 +3,7 @@ import React from "react";
 function SiteSummary() {
   return (
     <div className="siteSummary">
-      <p>Welcome to Archivance, the unofficial archive of Canadian technical
+      <p>Welcome to Archivance, the unofficial archive of technical
         clothing brand Veilance.</p>
       <p>Select a season above to get started.</p>
     </div>
